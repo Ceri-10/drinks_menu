be575ec9b78ed84c561b7cd5c0eb67d92f153c78
Ceri-10 <alberto.canovas@hotmail.com>
2024-11-06 19:09:03 +0100

    Init commit

---
+++ src/router.tsx
@@ -0,0 +1,11 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+export default function router() {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path="/"/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
